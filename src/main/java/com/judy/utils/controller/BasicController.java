7652ce7c97646f56af9cb0a59361cb04e38b9e45
Judy <devzoou@gmail.com>
2024-01-23 16:52:14 +0900

    feat: pingpong rest controller 추가

--- src/main/java/com/judy/utils/controller/BasicController.java
+++ src/main/java/com/judy/utils/controller/BasicController.java
@@ -1,12 +1,22 @@
 package com.judy.utils.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Slf4j
 public class BasicController {
+
+    /**
+     * ping pong
+     */
+    @GetMapping("/ping")
+    public ResponseEntity<String> pingpong() {
+        return ResponseEntity.ok("pong");
+    }
 
     /**
      * Main
